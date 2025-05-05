e09eb6c22583563d1fb3b3dff5f41944e4d2726e
farazalam27 <farazalam27@gmail.com>
2025-05-04 22:56:04 -0400

    Adjusted form width, hopefully fixed on mobile

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -22,9 +22,9 @@ export const Contact = () => {
             .catch(() => alert('Oops! Something went wrong. Please try again.'));
     }
     return (
-        <section id="contact" className="min-h-screen max-w-screen flex items-center justify-center py-20">
+        <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
-                <div className="px-4 w-150">
+                <div className="px-4 w-full sm:w-200">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                         Get In Touch
                     </h2>
