3ded52512a32ec2ebd291866e8242f5ccd074147
farazalam27 <farazalam27@gmail.com>
2025-05-22 12:49:48 -0400

    Updated and removed some files, redirecting to fqa.info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                     {" "}
-                    fqa<span className="text-blue-500">.io</span>{" "}
+                    fqa<span className="text-blue-500">.info</span>{" "}
                 </a>
 
                 <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
